5ecdab7d2cd6580309b8119f1a2c0cc1498a2917
akileshas <akileshas.ai@gmail.com>
2025-05-18 02:14:11 +0530

    written my first cuda kernel !

---
+++ 001x/hello.cu
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+using namespace std;
+
+// create a kernel function
+__global__ void ping() {
+    printf("ping from [block: %d](thread: %d)\n", blockIdx.x, threadIdx.x);
+}
+
+
+int main() {
+    // <<< numBlocks, numThreadsPerBlock >>>
+    ping<<<2,5>>>();
+
+    // used to join and synchronize threads
+    cudaDeviceSynchronize();
+    return 0;
+}
